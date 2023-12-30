04dac3f646cc1e75a5e61b0f25d7d9d14ba1d0eb
la-coolax <smekki160304@gmail.com>
2023-12-30 23:44:18 +0100

    test

--- entrepot/models.py
+++ entrepot/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.db.models import F
 
+#test
 
 class Fournisseur(models.Model):
     code = models.CharField(max_length=20)
